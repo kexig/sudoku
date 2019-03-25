dece30a182c9ba56cbaf87e2b9db1d6a371f828f
Emil Johansson <emil.jhnsn@gmail.com>
2019-03-25 17:42:36 +0100

    Refactor Grid slightly

--- SudokuHelp/Grid9x9.cs
+++ SudokuHelp/Grid9x9.cs
@@ -11,28 +11,32 @@ namespace SudokuHelp
 {
     public partial class Grid9x9 : UserControl
     {
-        private Tile[,] Tiles;
+        private int Rows;
+        private int Columns;
 
         public Grid9x9()
         {
             InitializeComponent();
+            Rows = LayoutPanel.RowCount;
+            Columns = LayoutPanel.ColumnCount;
         }
-        //public int[,] Value
-        //{
-        //    get
-        //    {
-        //        int[,] array1 = new int[Tiles.GetLength(0), Tiles.GetLength(1)];
-        //        for (int Row = 0; Row < Tiles.GetLength(0); Row++)
-        //        {
-        //            for (int Column = 0; Column < Tiles.GetLength(1); Column++)
-        //            {
-        //                Tile Tile = (Tile)SudokuTableLayoutPanel.GetControlFromPosition(Column, Row);
-        //                ValueOfTiles.Add(Tile.Value);
-        //            }
-        //        }
-        //        return;
-        //    }
-        //}
+
+        public int[,] Value
+        {
+            get
+            {
+                int[,] Result = new int[Rows, Columns];
+                for (int Row = 0; Row < Rows; Row++)
+                {
+                    for (int Column = 0; Column < Columns; Column++)
+                    {
+                        Tile tile = (Tile)LayoutPanel.GetControlFromPosition(Column, Row);
+                        Result[Row, Column] = tile.Value;
+                    }
+                }
+                return Result;
+            }
+        }
 
         public void Populate(int[,] Value)
         {
@@ -40,7 +44,7 @@ namespace SudokuHelp
             {
                 for (int Column = 0; Column < Value.GetLength(1); Column++)
                 {
-                    Tile Tile = (Tile)SudokuTableLayoutPanel.GetControlFromPosition(Column, Row);
+                    Tile Tile = (Tile)LayoutPanel.GetControlFromPosition(Column, Row);
                     Tile.Row = Row;
                     Tile.Column = Column;
                     Tile.Maximum = 9;
