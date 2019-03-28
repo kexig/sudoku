09ec3ec568ed0d9914c2785f5240099516e7c4b1
Emil Johansson <emil.jhnsn@gmail.com>
2019-03-28 19:19:46 +0100

    Verify function working

--- SudokuHelp/ClassicSudoku.cs
+++ SudokuHelp/ClassicSudoku.cs
@@ -7,18 +7,42 @@ namespace SudokuHelp
 {
     public class ClassicSudoku
     {
-        private List<SudokuTile> Tiles;
+        private List<SudokuTile> Tiles = new List<SudokuTile>();
+        private int Columns;
+        private int Rows;
+        private List<IGrouping<int, SudokuTile>> GroupByColumn;
+        private List<IGrouping<int, SudokuTile>> GroupByRow;
+        private List<IEnumerable<SudokuTile>> GroupBy3x3 = new List<IEnumerable<SudokuTile>>();
+
 
         private void CreateTiles(int[,] Template)
         {
             for (int Row = 0; Row < Template.GetLength(0); Row++)
             {
-                for (int Column = 0; Row < Template.GetLength(1); Column++)
+                for (int Column = 0; Column < Template.GetLength(1); Column++)
                 {
                     SudokuTile Tile = new SudokuTile(Row, Column, 9);
                     Tile.Value = Template[Row, Column];
                     Tile.Fix = Template[Row, Column] == 0 ? false : true;
                     Tiles.Add(Tile);
+                }
+            }
+
+            GroupByColumn = Tiles.GroupBy(tile => tile.Column).ToList();
+            GroupByRow = Tiles.GroupBy(tile => tile.Row).ToList();
+            Columns = GroupByColumn.Count();
+            Rows = GroupByRow.Count();
+            //Rows = Tiles.Max(tile => tile.Row) + 1;
+
+            int MiniSquareSize = 3;
+            GroupBy3x3.Clear();
+            for (int Row = 0; Row < (Rows / MiniSquareSize); Row++)
+            {
+                for (int Column = 0; Column < (Columns / MiniSquareSize); Column++)
+                {
+                    var TilesInRange = Tiles.Where(tile => (tile.Row < (Row * MiniSquareSize)) && (tile.Row >= (Row * MiniSquareSize - MiniSquareSize)))
+                        .Where(tile => (tile.Column < (Column * MiniSquareSize)) && (tile.Column >= (Column * MiniSquareSize - MiniSquareSize)));
+                    GroupBy3x3.Add(TilesInRange);
                 }
             }
         }
@@ -38,12 +62,12 @@ namespace SudokuHelp
 
         public ClassicSudoku(int[,] Template)
         {
-
+            CreateTiles(Template);
         }
 
         public void Populate(int[,] Template)
         {
-
+            CreateTiles(Template);
         }
 
         public void Populate()
@@ -54,6 +78,47 @@ namespace SudokuHelp
         public void Solve()
         {
 
+        }
+
+        public bool Verify()
+        {
+            foreach (var Column in GroupByColumn)
+            {
+                bool AllUnique = Column.GroupBy(x => x.Value).All(g => g.Count() == 1);
+                switch (AllUnique)
+                {
+                    case true:
+                        break;
+                    case false:
+                        return false;
+                }
+            }
+
+            foreach (var Row in GroupByRow)
+            {
+                bool AllUnique = Row.GroupBy(x => x.Value).All(g => g.Count() == 1);
+                switch (AllUnique)
+                {
+                    case true:
+                        break;
+                    case false:
+                        return false;
+                }
+            }
+
+            foreach (var Square in GroupBy3x3)
+            {
+                bool AllUnique = Square.GroupBy(x => x.Value).All(g => g.Count() == 1);
+                switch (AllUnique)
+                {
+                    case true:
+                        break;
+                    case false:
+                        return false;
+                }
+            }
+
+            return true;
         }
     }
 }
---
+++ SudokuHelpTests/ClassicSudokuTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuHelp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuHelp.Tests
+{
+    [TestClass()]
+    public class ClassicSudokuTests
+    {
+        [TestMethod()]
+        public void VerifyTest()
+        {
+            int[,] Solved = new int[9, 9]
+                {
+                    { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                    { 7, 8, 9, 1, 2, 3, 4, 5, 6 },
+                    { 4, 5, 6, 7, 8, 9, 1, 2, 3 },
+                    { 3, 1, 2, 8, 4, 5, 9, 6, 7 },
+                    { 6, 9, 7, 3, 1, 2, 8, 4, 5 },
+                    { 8, 4, 5, 6, 9, 7, 3, 1, 2 },
+                    { 2, 3, 1, 5, 7, 4, 6, 9, 8 },
+                    { 9, 6, 8, 2, 3, 1, 5, 7, 4 },
+                    { 5, 7, 4, 9, 6, 8, 2, 3, 1 }
+                };
+            ClassicSudoku Sudoko = new ClassicSudoku(Solved);
+            
+            if (!Sudoko.Verify())
+            {
+                Assert.Fail("Solved sudoku returning false");
+            }
+
+
+            int[,] Wrong = new int[9, 9]
+                {
+                    { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                    { 7, 8, 9, 1, 2, 3, 4, 5, 6 },
+                    { 4, 5, 6, 7, 8, 9, 1, 2, 3 },
+                    { 3, 1, 2, 8, 4, 5, 9, 6, 7 },
+                    { 6, 9, 7, 3, 1, 2, 8, 4, 5 },
+                    { 8, 4, 5, 6, 9, 7, 3, 1, 2 },
+                    { 2, 3, 1, 5, 7, 4, 6, 9, 8 },
+                    { 9, 6, 8, 2, 3, 1, 5, 7, 4 },
+                    { 5, 7, 4, 9, 6, 8, 2, 3, 3 } // 3 at the end should be 1
+                };
+            Sudoko = new ClassicSudoku(Wrong);
+
+            if (Sudoko.Verify())
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
